e4bc0160967f2d6aac8bec510f7c5b9ce576a6bb
Sara Walker <sarawall1187@gmail.com>
2019-06-18 14:59:43 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,7 @@ require 'csv'
 
   def upload
     CSV.foreach(params[:songs].path, headers: true) do |song|
-      Song.create(title: song[0])
+      Song.create(title: song[0], artist_name: song[1])
     end
     redirect_to songs_path
   end
